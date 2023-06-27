6b40f333607fc5945f10d6b4358a56dbca8ed5f5
Pierre <Pierre@DESKTOP-UJADA9E>
2023-06-27 12:16:52 -0500

    Agregar archivos de proyecto.

---
+++ PromedioCalificaciones/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace PromedioCalificaciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {    // Asignación compuesta (x -= 5) == (x = x - 5)
+             // acumulador = acumulador + variable) == (acumulador += variable
+             //variables
+
+            byte i, numAlumnos;
+            double  calificacion, sumaCalif = 0, promedio;
+
+            Console.Write("Por favor ingresa el numero de alumnos:  ");
+            numAlumnos = Convert.ToByte(Console.ReadLine());
+
+            for(i = 0; i < numAlumnos; i++) 
+            {
+                Console.Write("Por favor ingresa la calificacion:  ");
+                calificacion = Convert.ToDouble(Console.ReadLine());
+
+               sumaCalif += calificacion;
+            }
+            promedio = sumaCalif / numAlumnos;
+            Console.WriteLine("El promedio del curso es {0}",promedio);
+
+
+
+
+
+        }
+    }
+}
